d149d63e6af43b6c95a5a0458b03d9367ba4957c
lnkssr <gifzoner5@gmail.com>
2024-09-12 15:57:55 +0300

    add proxy cheker

--- proxy_parcer/main.py
+++ proxy_parcer/main.py
@@ -1,12 +1,22 @@
 import requests
-import socket
 
-def check_proxy(ip, port, timeout=5):
+def check_http_proxy(ip, port, timeout=5):
+    proxies = {
+        'http': f'http://{ip}:{port}',
+        'https': f'http://{ip}:{port}'
+    }
+
     try:
-        with socket.create_connection((ip, port), timeout):
+        # Проверяем доступность сайта
+        response = requests.get('http://google.com', proxies=proxies, timeout=timeout)
+        if response.status_code == 200:
+            print(f"Прокси работает. Ответ от httpbin.org: {response.text}")
             return True
-    except (socket.timeout, socket.error):
-        return False
+        else:
+            print(f"Прокси не работает. Код ответа: {response.status_code}")
+    except requests.RequestException as e:
+        print(f"Ошибка запроса: {e}")
+    return False
 
 def download_proxy_list(url):
     response = requests.get(url)
@@ -23,15 +33,14 @@ def validate_proxies(proxy_list, output_file):
                 continue
             try:
                 ip, port = line.split(':')
-                if check_proxy(ip, int(port)):
-                    print(f"Прокси активен: {line}")
+                if check_http_proxy(ip, int(port)):
                     f.write(line + '\n')  # Запись в файл в реальном времени
                 else:
                     print(f"Прокси не активен: {line}")
             except ValueError:
                 print(f"Некорректная строка: {line}")
 
-url = 'https://raw.githubusercontent.com/TheSpeedX/SOCKS-List/master/socks4.txt'
+url = 'https://raw.githubusercontent.com/TheSpeedX/SOCKS-List/master/http.txt'
 output_file = 'active_proxies.txt'
 
 proxy_list = download_proxy_list(url)
